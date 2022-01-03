a5b3b1ef63d437397ad07a36efe78184039d8c6d
odilovalisher17 <odilovalisher17@gmail.com>
2022-01-03 23:37:21 +0500

    update4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import AppNavbar from './Components/AppNavbar/AppNavbar'
 import Homepage from './Components/HomePage/Homepage'
+import About from './Components/About/About'
 
 const App = () => {
   return (
     <Router>
       <AppNavbar />
       <Routes>
-        <Route  path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </Router>
   )
---
+++ src/Components/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './About.css'
+import foto1 from './programmer.png'
+
+const About = () => {
+    return (
+        <div className='about'>
+            <div className='about-left'>
+                <img src={foto1} alt="" />
+                <h3>My Experience</h3>
+            </div>
+            
+            <div className='about-right'>
+                <h2>About Me</h2>
+                <p>
+                    I am 20 y.o Front-End developer located in Tashkent Uzbekistan. I am a student of Tashkent State Technical University. I know technologies HTML, CSS, Bootstrap, JasaScript(ES6), ReactJS, Redux and Rest API. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
