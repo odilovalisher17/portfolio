75224b9db64ed188c070c9422c3d34481c399e05
odilovalisher17 <odilovalisher17@gmail.com>
2022-01-05 21:27:26 +0500

    update 6

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -16,8 +16,15 @@ const About = () => {
                 <h2>About Me</h2>
                 <br /> <hr /> <br />
                 <p>
-                    I am 20 y.o Front-End developer located in Tashkent Uzbekistan. I am a student of Tashkent State Technical University. I know technologies HTML, CSS, Bootstrap, JasaScript(ES6), ReactJS, Redux and Rest API. 
+                    I am 20 y.o Front-End developer located in Tashkent Uzbekistan. I am a student of Tashkent State Technical University. I know technologies HTML, CSS, Bootstrap, JasaScript(ES6), ReactJS, Redux and Rest API.
                 </p>
+
+                <div className='about-icons'>
+                    <i style={{color:"black"}} class="fa fa-github"></i>
+                    <i style={{color:"blue"}} class="fa fa-telegram"></i>
+                    <i class="fa fa-phone"></i>
+                    <i style={{color:'rgb(230, 29, 96)'}} class="fa fa-instagram"></i>
+                </div>
 
                 <button className='h-btn'>Download Resume</button>
             </div>
